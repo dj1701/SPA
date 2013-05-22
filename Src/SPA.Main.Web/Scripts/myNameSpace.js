e10cb660c8d19d8f950ff266922fa459b1f7f6fe
DJ <omegaman912000-uk@yahoo.co.uk>
2013-05-22 23:06:46 +0100

    DJ: Sample Javascipt class

---
+++ Src/SPA.Main.Web/Scripts/myNameSpace.js
@@ -0,0 +1,22 @@
+﻿var myNamespace = myNamespace || {};
+
+myNamespace.Customer = function (firstName, lastName, phoneNumbers) {
+    this.Firstname = firstName;
+    this.Lastname = lastName;
+    this.PhoneNumbers = phoneNumbers || [];
+
+    var getMobileNumber = function () {
+        for (var x = 0; x <= PhoneNumbers.length; x++) {
+            if (PhoneNumbers[x].Type == "Mobile")
+                return PhoneNumbers[x];
+
+        }
+        return "No Mobile Number";
+    };
+};
+
+myNamespace.PhoneNumber = function (type, number) {
+    this.Type = type;
+    this.Number = number;
+};
+
