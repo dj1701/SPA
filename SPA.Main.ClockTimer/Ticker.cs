f72e8aaeba3bbf54dca644eb9ddd878f0676efa3
David Jacobs <david.jacobs@bglgroup.co.uk>
2013-05-28 17:43:45 +0100

    DJ: Added test for timer for check time span between start and finish times

---
+++ SPA.Main.ClockTimer/Ticker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace SPA.Main.RefreshTimer
+{
+    public class Ticker
+    {
+        private readonly Timer timer = new Timer();
+        private int hour;
+        private int minute;
+        private int second;
+
+        public string FinishTime { get; private set; }
+
+        public void Start()
+        {
+            timer.Elapsed += new ElapsedEventHandler(TimerElapsed);
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            hour = e.SignalTime.Hour;
+            minute = e.SignalTime.Minute;
+            second = e.SignalTime.Second;
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            FinishTime = String.Format("{0:D2}:{1:D2}:{2:D2}", hour, minute, second);
+            timer.Elapsed -= TimerElapsed;
+            timer.Enabled = false;
+        }
+    }
+}
---
+++ SPA.Main.Tests/RefreshTimerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using SPA.Main.RefreshTimer;
+
+namespace SPA.Main.Tests
+{
+    [TestFixture]
+    public class RefreshTimerTests
+    {
+        [Test]
+        public void ShouldHavePreciseTimeSpanBetweenStartTimeAndFinishTime()
+        {
+            var ticker = new Ticker();
+            var timeSpan = new TimeSpan(0, 0, 0, 10);
+            var expectedFinishTime = DateTime.Now.Add(timeSpan);
+            const int secondsTimeSpan = 11000;
+
+            ticker.Start();
+            Thread.Sleep(secondsTimeSpan);
+            ticker.Stop();
+
+            var expectedFinishTimeFormatted = String.Format("{0:D2}:{1:D2}:{2:D2}", expectedFinishTime.Hour, expectedFinishTime.Minute, expectedFinishTime.Second);
+            Assert.IsNotNull(ticker.FinishTime);
+            Assert.That(expectedFinishTimeFormatted,Is.EqualTo(ticker.FinishTime));
+        }
+    }
+}
