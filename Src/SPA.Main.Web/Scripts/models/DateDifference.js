e54a33615ba547051603012437b4da1173a4c7d6
DJ <omegaman912000-uk@yahoo.co.uk>
2014-04-21 19:16:36 +0100

    DJ: refactored out some code duplication into reusable function.

--- Src/SPA.Main.Web/Scripts/models/DateDifference.js
+++ Src/SPA.Main.Web/Scripts/models/DateDifference.js
@@ -5,32 +5,36 @@
 
     var oneDay = 1000 * 60 * 60 * 24;
 
+    var getDifference = function() {
+        return self.secondDate - self.firstDate;
+    };
+
     self.Years = function () {
-        var difference = self.secondDate - self.firstDate;
+        var difference = getDifference();
         var years = Math.floor((difference / oneDay) % 1.5 == 1 ? (difference / oneDay) / 365.24 : (difference / oneDay) / 365);
         return years;
     };
 
     self.Months = function () {
-        var difference = self.secondDate - self.firstDate;
+        var difference = getDifference();
         var months = Math.floor(((difference / oneDay) / 365 * 12) % 1 == 0 ? ((difference / oneDay) / 365 * 12) : ((difference / oneDay) / 365.24 * 12));
         return months;
     };
 
     self.Days = function (includeLastDay) {
-        var difference = self.secondDate - self.firstDate;
+        var difference = getDifference();
         var days = Math.round(difference / oneDay) + (includeLastDay ? 1 : 0);
         return days;
     };
 
     self.Hours = function (includeLastDay) {
-        var difference = self.secondDate - self.firstDate;
+        var difference = getDifference();
         var hours = (Math.floor(difference / oneDay) * 24) + (includeLastDay ? 24 : 0);
         return hours;
     };
 
     self.Minutes = function (includeLastDay) {
-        var difference = self.secondDate - self.firstDate;
+        var difference = getDifference();
         var minutes = (Math.floor(difference / oneDay) * 24 + (includeLastDay ? 24 : 0)) * 60;
         return minutes;
     };
