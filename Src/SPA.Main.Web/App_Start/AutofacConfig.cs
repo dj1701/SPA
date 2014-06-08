7c2f3d72b05fd3560cd19ace7091eb4be28107aa
DJ <omegaman912000-uk@yahoo.co.uk>
2014-06-08 19:45:14 +0100

    DJ: Inclusion of jasmine 2.0.0.  Jasine 1.3.1 remains part of the solution.  Have included Autofac config file.

---
+++ Src/SPA.Main.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using SPA.Main.RefreshTimer;
+
+namespace SPA.Main.Web.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Ticker>().As<ITicker>().InstancePerHttpRequest();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly()).InstancePerHttpRequest();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
