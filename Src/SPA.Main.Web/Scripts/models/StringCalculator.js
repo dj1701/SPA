d6e1d5271342a0c167dee3f05573255c547f39c3
DJ <omegaman912000-uk@yahoo.co.uk>
2014-07-01 22:31:28 +0100

    DJ: Code Kata String Calculator in Javascript

---
+++ Src/SPA.Main.Web/Scripts/models/StringCalculator.js
@@ -0,0 +1,44 @@
+﻿function StringCalculator() {
+    var self = this;
+
+    self.Add = function (numbers) {
+        if (numbers == "") return 0;
+
+        var separatedNumbers = numbers.split(',');
+
+        var sumOfAllNumbers = 0;
+        for (var i = 0; i < separatedNumbers.length; i++) {
+            sumOfAllNumbers += separatedNumbers[i] | 0;
+        }
+
+//        var sumOfAllNumbers = separatedNumbers.map(function (ele) {
+//            return /^\d+$/.test(ele) ? parseInt(ele) : 0;
+//        }).reduce(function (previous, current) {
+//            return previous + current;
+//        });
+
+        return sumOfAllNumbers;
+    };
+
+}
+
+
+var singleton = (function () {
+
+    var instance;
+
+    var createInstance = function () {
+        var strCalc = new StringCalculator();
+        return strCalc;
+    };
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+
+            return instance;
+        }
+    };
+})();
---
+++ Src/SPA.Main.Web/Scripts/specs/StringCalculatorSpec.js
@@ -0,0 +1,51 @@
+﻿describe('String Calculator', function () {
+    //The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
+    it('Should return the sum of 0 if empty string is provided', function () {
+        var numbers = "";
+
+        var instance = singleton.getInstance();
+
+        var result = instance.Add(numbers);
+
+        expect(result).toBe(0);
+    });
+
+    it('Should return the sum of 1 if 0,1 is provided', function () {
+        var numbers = "0,1";
+
+        var instance = singleton.getInstance();
+
+        var result = instance.Add(numbers);
+
+        expect(result).toBe(1);
+    });
+
+    it('Should return the sum of 2 if 0,2 is provided', function () {
+        var numbers = "0,2";
+
+            var instance = singleton.getInstance();
+
+        var result = instance.Add(numbers);
+
+        expect(result).toBe(2);
+    });
+
+    it('Should return the sum of 6 if 1,2,3 is provided', function () {
+        var numbers = "1,2,3";
+
+        var instance = singleton.getInstance();
+
+        var result = instance.Add(numbers);
+
+        expect(result).toBe(6);
+    });
+});
+
+describe('Singleton', function () {
+    it('Should return same instance of method', function () {
+        var instance1 = singleton.getInstance();
+        var instance2 = singleton.getInstance();
+
+        expect(instance1).toEqual(instance2);
+    });
+});
