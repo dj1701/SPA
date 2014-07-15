7ba250e296090e814aaac00df562c8fd3ca26243
David Jacobs <david.jacobs@bglgroup.co.uk>
2014-07-15 08:59:29 +0100

    DJ: Amendment to include revealing prototype pattern

--- Src/SPA.Main.Web/Scripts/models/StringCalculator.js
+++ Src/SPA.Main.Web/Scripts/models/StringCalculator.js
@@ -1,13 +1,23 @@
-﻿function StringCalculator() {
-    var self = this;
+﻿var StringCalculator = function ()
+{
 
-    self.Add = function (numbers) {
+};
+
+StringCalculator.prototype = function () {
+
+    var validPattern = function(param) {
+        var re = /((\d(\\n|,|;){1})+\d)+/g;
+
+        return param.match(re) !== null;
+    },
+
+    add = function (numbers) {
         if ((numbers | 0) < 0) throw new Error("negatives not allowed");
 
         if (!validPattern(numbers)) return 0;
 
         var sumOfAllNumbers = 0;
-        numbers = numbers.replace(/(\d{4})*/g,'');
+        numbers = numbers.replace(/(\d{4})*/g, '');
         var multipleDigits = numbers.match(/(\d{3}|\d{2}|\d{1})*/g);
         if (multipleDigits) {
             for (var i = 0; i < multipleDigits.length; i++) {
@@ -17,14 +27,11 @@
 
         return sumOfAllNumbers;
     };
-
-    var validPattern = function(param) {
-        var re = /((\d(\\n|,|;){1})+\d)+/g;
-
-        return param.match(re) !== null;
+    
+    return {        
+        Add: add
     };
-}
-
+}();
 
 var singleton = (function () {
 
