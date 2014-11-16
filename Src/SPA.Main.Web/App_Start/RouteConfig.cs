a25e2f30db270d64fdc68beb75af5c4e150fbca7
DJ <omegaman912000-uk@yahoo.co.uk>
2014-11-16 22:14:26 +0000

    DJ: - Tidy up spec runner files into dedicated folder.  Set default view.

--- Src/SPA.Main.Web/App_Start/RouteConfig.cs
+++ Src/SPA.Main.Web/App_Start/RouteConfig.cs
@@ -10,16 +10,16 @@ namespace SPA.MainUI.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            //routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
