eab8c041714511e2e707ef325baf520b6db1291e
DJ <omegaman912000-uk@yahoo.co.uk>
2015-02-23 22:07:22 +0000

    <DJ> Update to Time Span Test. Test for the server based timer.

--- Src/SPA.Main.Tests/RefreshTimerTests.cs
+++ Src/SPA.Main.Tests/RefreshTimerTests.cs
@@ -14,14 +14,14 @@ namespace SPA.Main.Tests
             var ticker = new Ticker();
             var timeSpan = new TimeSpan(0, 0, 0, 10);
             var expectedFinishTime = DateTime.Now.Add(timeSpan);
-            const int secondsTimeSpan = 10500;
+            const int secondsTimeSpan = 10200;
 
             ticker.Start();
             Thread.Sleep(secondsTimeSpan);
             ticker.Stop();
 
-            //var expectedFinishTimeFormatted = String.Format("{0:D2}:{1:D2}:{2:D2}", expectedFinishTime.Hour, expectedFinishTime.Minute, expectedFinishTime.Second);
-            var expectedFinishTimeFormatted = String.Format("{0:D2}:{1:D2}", expectedFinishTime.Hour, expectedFinishTime.Minute);
+            var expectedFinishTimeFormatted = String.Format("{0:D2}:{1:D2}:{2:D2}", expectedFinishTime.Hour, expectedFinishTime.Minute, expectedFinishTime.Second);
+            //var expectedFinishTimeFormatted = String.Format("{0:D2}:{1:D2}", expectedFinishTime.Hour, expectedFinishTime.Minute);
             Assert.IsNotNull(ticker.Time);
             Assert.That(expectedFinishTimeFormatted,Is.EqualTo(ticker.Time));
         }
